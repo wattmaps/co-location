aea8648a83b79a125fedf7785554e48603173f3b
avidalmeza <vidalmeza.alessandra0@gmail.com>
2024-06-16 13:33:33 -0400

    Add if/else for year, phaseout, and scenario

--- 03_GEA_Cash_Flow.py
+++ 03_GEA_Cash_Flow.py
@@ -15,72 +15,104 @@ gea_region_codes = ['CAMXc', 'NWPPc', 'AZNMc', 'RMPAc', 'ERCTc', 'SPSOc', 'SPNOc
                     'MROWc', 'MROEc', 'SRMWc', 'SRMVc', 'SRTVc', 'SRSOc', 'FRCCc',
                     'RFCWc', 'RFCMc', 'SRVCc', 'RFCEc', 'NYSTc', 'NEWEc']
 
+# Set year, phaseout, and scenario
+# year = 2025
+year = 2030
+ptc_phaseout = True
+# ptc_phaseout = False
+# scenario = 'Mid-case'
+scenario = 'Electrification'
+
 ''' ============================
 For loop by GEA region
 ============================ '''
 
-for i in gea_region_codes:
-
-    gea = i
+for gea in gea_region_codes:
 
     # Define production tax credit (in USD per MWh)
     PTC_wind = 26 
 
-    # Set filepath for folder
-    ePrice_df_folder = os.path.join(inputFolder, 'Cambium22_Mid-case', 'input')
-    # ePrice_df_folder = os.path.join(inputFolder, 'Cambium22_Electrification', 'input')
-    
-    # Set filepath for csv
-    ePrice_path_t1 = ePrice_df_folder + '/Cambium22_MidCase_hourly_' + gea + '_2024.csv'
-    ePrice_path_t2 = ePrice_df_folder + '/Cambium22_MidCase_hourly_' + gea + '_2026.csv'
-    ePrice_path_t3 = ePrice_df_folder + '/Cambium22_MidCase_hourly_' + gea + '_2028.csv'
-    ePrice_path_t4 = ePrice_df_folder + '/Cambium22_MidCase_hourly_' + gea + '_2030.csv'
-    ePrice_path_t5 = ePrice_df_folder + '/Cambium22_MidCase_hourly_' + gea + '_2035.csv'
-    ePrice_path_t6 = ePrice_df_folder + '/Cambium22_MidCase_hourly_' + gea + '_2040.csv'
-    ePrice_path_t7 = ePrice_df_folder + '/Cambium22_MidCase_hourly_' + gea + '_2045.csv'
-    ePrice_path_t8 = ePrice_df_folder + '/Cambium22_MidCase_hourly_' + gea + '_2050.csv'
+    # Define filepath for scenario folder and files
+    folderScenario = f'Cambium22_{scenario}_2030' if year == 2030 else f'Cambium22_{scenario}'
+    # Correct for ePrice_paths; update 'Mid-case' to 'MidCase' for input data
+    scenario_filepath = f'Cambium22_{scenario}' if scenario != 'Mid-case' else 'Cambium22_MidCase'
 
-    # ePrice_path_t1 = ePrice_df_folder + '/Cambium22_Electrification_hourly_' + gea + '_2024.csv'
-    # ePrice_path_t2 = ePrice_df_folder + '/Cambium22_Electrification_hourly_' + gea + '_2026.csv'
-    # ePrice_path_t3 = ePrice_df_folder + '/Cambium22_Electrification_hourly_' + gea + '_2028.csv'
-    # ePrice_path_t4 = ePrice_df_folder + '/Cambium22_Electrification_hourly_' + gea + '_2030.csv'
-    # ePrice_path_t5 = ePrice_df_folder + '/Cambium22_Electrification_hourly_' + gea + '_2035.csv'
-    # ePrice_path_t6 = ePrice_df_folder + '/Cambium22_Electrification_hourly_' + gea + '_2040.csv'
-    # ePrice_path_t7 = ePrice_df_folder + '/Cambium22_Electrification_hourly_' + gea + '_2045.csv'
-    # ePrice_path_t8 = ePrice_df_folder + '/Cambium22_Electrification_hourly_' + gea + '_2050.csv'
+    # Set filepath for folder
+    ePrice_df_folder = os.path.join(inputFolder, folderScenario, 'input')
+
+    # Define file paths for 2025
+    if year == 2025:
+        ePrice_paths = [
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2024.csv'), # i = 0
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2026.csv'), # i = 1
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2028.csv'), # i = 2
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2030.csv'), # i = 3
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2035.csv'), # i = 4
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2040.csv'), # i = 5
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2045.csv'), # i = 6
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2050.csv') # i = 7
+        ]
+    # Define file paths for 2030
+    else:
+        ePrice_paths = [
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2024.csv'), # i = 0
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2026.csv'), # i = 1
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2028.csv'), # i = 2
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2030.csv'), # i = 3
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2035.csv'), # i = 4
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2040.csv'), # i = 5
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2045.csv'), # i = 6
+            os.path.join(ePrice_df_folder, f'{scenario_filepath}_hourly_{gea}_2050.csv') # i = 7
+        ]
 
     # Read csv and remove first 5 rows
-    ePrice_df_t1 = pd.read_csv(ePrice_path_t1, skiprows = 5)
-    ePrice_df_t2 = pd.read_csv(ePrice_path_t2, skiprows = 5)
-    ePrice_df_t3 = pd.read_csv(ePrice_path_t3, skiprows = 5)
-    ePrice_df_t4 = pd.read_csv(ePrice_path_t4, skiprows = 5)
-    ePrice_df_t5 = pd.read_csv(ePrice_path_t5, skiprows = 5)
-    ePrice_df_t6 = pd.read_csv(ePrice_path_t6, skiprows = 5)
-    ePrice_df_t7 = pd.read_csv(ePrice_path_t7, skiprows = 5)
-    ePrice_df_t8 = pd.read_csv(ePrice_path_t8, skiprows = 5)
-    # ePrice_df.columns.values
+    ePrice_dfs = [pd.read_csv(i, skiprows = 5) for i in ePrice_paths]
 
-    # Extract 'total_cost_enduse' column
-    ePrice_df_t1 = ePrice_df_t1['total_cost_enduse'] + PTC_wind 
-    ePrice_df_t2 = ePrice_df_t2['total_cost_enduse'] + PTC_wind 
-    ePrice_df_t3 = ePrice_df_t3['total_cost_enduse'] + PTC_wind 
-    ePrice_df_t4 = ePrice_df_t4['total_cost_enduse'] + PTC_wind 
-    ePrice_df_t5 = ePrice_df_t5['total_cost_enduse']
-    ePrice_df_t6 = ePrice_df_t6['total_cost_enduse']
-    ePrice_df_t7 = ePrice_df_t7['total_cost_enduse']
-    ePrice_df_t8 = ePrice_df_t8['total_cost_enduse']
+    # Apply production tax credit with phaseout where ptc_phaseout = True
+    if ptc_phaseout:
+        # enumerate() provides both index (i) and dataframe (df) to iterate over list of ePrice_dfs
+        for i, df in enumerate(ePrice_dfs):
+            # If year == 2025, then PTC is added to first four dfs/first 10 years
+            # If year == 2030, then PTC is added to first five dfs/first 10 years
+            if (year == 2025 and i < 4) or (year == 2030 and i < 5):
+                df['total_cost_enduse'] += PTC_wind
+    # Apply production tax credit with no phaseout where ptc_phaseout = False
+    else:
+        for df in ePrice_dfs:
+            # Add PTC to all years
+            df['total_cost_enduse'] += PTC_wind
 
-    # Concatenate 
-    ePrice_2026_27_df = pd.concat([ePrice_df_t2, ePrice_df_t2], axis = 1, ignore_index = True)
-    ePrice_2028_29_df = pd.concat([ePrice_df_t3, ePrice_df_t3], axis = 1, ignore_index = True)
-    ePrice_2030_34_df = pd.concat([ePrice_df_t4, ePrice_df_t4, ePrice_df_t4, ePrice_df_t4, ePrice_df_t4], axis = 1, ignore_index = True)
-    ePrice_2035_39_df = pd.concat([ePrice_df_t5, ePrice_df_t5, ePrice_df_t5, ePrice_df_t5, ePrice_df_t5], axis = 1, ignore_index = True)
-    ePrice_2040_44_df = pd.concat([ePrice_df_t6, ePrice_df_t6, ePrice_df_t6, ePrice_df_t6, ePrice_df_t6], axis = 1, ignore_index = True)
-    ePrice_2045_49_df = pd.concat([ePrice_df_t7, ePrice_df_t7, ePrice_df_t7, ePrice_df_t7, ePrice_df_t7], axis = 1, ignore_index = True)
+    # Extract 
+    ePrice_dfs_vec = [df['total_cost_enduse'] for df in ePrice_dfs]
 
-    # Concatenate
-    ePrice_df = pd.concat([ePrice_df_t1, ePrice_2026_27_df, ePrice_2028_29_df, ePrice_2030_34_df, ePrice_2035_39_df, ePrice_2040_44_df, ePrice_2045_49_df], axis = 1, ignore_index = True)
-    
+    # Concatenate for 2025
+    if year == 2025:
+        ePrice_df = pd.concat(
+            [
+                ePrice_dfs_vec[0],              # ePrice_2025 
+                *([ePrice_dfs_vec[1]] * 2),     # ePrice_2026_27
+                *([ePrice_dfs_vec[2]] * 2),     # ePrice_2028_29
+                *([ePrice_dfs_vec[3]] * 5),     # ePrice_2030_34
+                *([ePrice_dfs_vec[4]] * 5),     # ePrice_2035_39
+                *([ePrice_dfs_vec[5]] * 5),     # ePrice_2040_44
+                *([ePrice_dfs_vec[6]] * 5)      # ePrice_2045_49
+            ], axis = 1, ignore_index = True
+        )
+    else:  # year == 2030
+        ePrice_df = pd.concat(
+            [
+                ePrice_dfs_vec[3],              # ePrice_2030_34
+                ePrice_dfs_vec[3],              # ePrice_2030_34
+                ePrice_dfs_vec[3],              # ePrice_2030_34
+                ePrice_dfs_vec[3],              # ePrice_2030_34
+                ePrice_dfs_vec[3],              # ePrice_2030_34
+                *([ePrice_dfs_vec[4]] * 5),     # ePrice_2035_39
+                *([ePrice_dfs_vec[5]] * 5),     # ePrice_2040_44
+                *([ePrice_dfs_vec[6]] * 5),     # ePrice_2045_49
+                *([ePrice_dfs_vec[7]] * 5)      # ePrice_2050_54
+            ], axis = 1, ignore_index = True
+        )
+  
     # Add hour column
     ePrice_df['hour'] = range(1, 8761)
     ePrice_df.insert(0, 'hour', ePrice_df.pop('hour'))
@@ -106,6 +138,8 @@ for i in gea_region_codes:
     ePrice_df_wide = pd.pivot(ePrice_df_long, index = 'hour', columns = 'year', values = 'PV_total_cost_enduse')
 
     # Save df to CSV file by GEA
-    folder = os.path.join(inputFolder, 'Cambium22_Mid-case', 'Cash_Flow')
-    # folder = os.path.join(inputFolder, 'Cambium22_Electrification', 'Cash_Flow')
+    folder = os.path.join(inputFolder, folderScenario, 'Cash_Flow_PTC_No_Phaseout')
+    # If PTC phaseout after 10 years
+    if ptc_phaseout:
+        folder = os.path.join(inputFolder, folderScenario, 'Cash_Flow')
     ePrice_df_wide.to_csv(f'{folder}/cambiumHourly_{gea}.csv')
