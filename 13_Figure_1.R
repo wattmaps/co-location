aec088243a5db0f62d404d87a3de48237b3df578
avidalmeza <vidalmeza.alessandra@gmail.com>
2024-09-13 12:31:37 -0700

    Update 12_Results_Data_Wrangling.R, 13_Figure_1.R

--- 13_Figure_1.R
+++ 13_Figure_1.R
@@ -38,7 +38,7 @@ ptc_scenarios <- subset_scenarios %>%
 fig_1_1 <- ptc_scenarios %>%
   ggplot(aes(y = solar_wind_ratio, group = scenario, fill = tx_availability)) + 
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
-  ylim(c(0.0, 2.0)) +
+  ylim(c(-0.15, 2.0)) +
   paletteer::scale_fill_paletteer_d('NatParksPalettes::Olympic') +
   labs(y = str_wrap('Ratio of solar to wind installed capacity', width = 12)) +
   theme_classic() +
@@ -51,7 +51,7 @@ fig_1_1 <- ptc_scenarios %>%
 fig_1_2 <- no_ptc_scenarios %>%
   ggplot(aes(y = solar_wind_ratio, group = scenario, fill = tx_availability)) + 
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
-  ylim(c(0.0, 2.0)) +
+  ylim(c(-0.15, 2.0)) +
   paletteer::scale_fill_paletteer_d('NatParksPalettes::Olympic') +
   labs(y = str_wrap('Ratio of solar to wind installed capacity', width = 12)) +
   theme_classic() +
@@ -63,11 +63,11 @@ fig_1_2 <- no_ptc_scenarios %>%
         axis.title.y = element_blank(),
         axis.text.y = element_blank())
 
-# Box and whisker plot for difference in co-location versus wind-only profits ----
+# Box and whisker plot for percent profit in co-location versus wind-only profits ----
 fig_1_3 <- ptc_scenarios %>%
-  ggplot(aes(y = diff_profit, group = scenario, fill = tx_availability)) + 
+  ggplot(aes(y = percent_profit, group = scenario, fill = tx_availability)) + 
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
-  ylim(c(-5.0, 10.0)) +
+  ylim(c(-75, 75)) +
   paletteer::scale_fill_paletteer_d('NatParksPalettes::Olympic') +
   labs(y = str_wrap('Difference in lifetime profit (USD/MWh)', width = 12)) +
   theme_classic() +
@@ -78,9 +78,9 @@ fig_1_3 <- ptc_scenarios %>%
         axis.text.x = element_blank())
 
 fig_1_4 <- no_ptc_scenarios %>%
-  ggplot(aes(y = diff_profit, group = scenario, fill = tx_availability)) + 
+  ggplot(aes(y = percent_profit, group = scenario, fill = tx_availability)) + 
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
-  ylim(c(-5.0, 10.0)) +
+  ylim(c(-75, 75)) +
   paletteer::scale_fill_paletteer_d('NatParksPalettes::Olympic') +
   labs(y = str_wrap('Difference in lifetime profit (USD/MWh)', width = 12)) +
   theme_classic() +
@@ -98,7 +98,7 @@ fig_1_5 <- ptc_scenarios %>%
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
   paletteer::scale_fill_paletteer_d('NatParksPalettes::Olympic') +
   labs(y = str_wrap('Annual average generation exported (MWh/yr)', width = 12)) +
-  scale_y_continuous(limits = c(0.00, 3000000.0), labels = scales::comma) +
+  scale_y_continuous(limits = c(0.00, 2000000.0), labels = scales::comma) +
   theme_classic() +
   theme(legend.position = 'non',
         legend.title = element_blank(),
@@ -111,7 +111,7 @@ fig_1_6 <- no_ptc_scenarios %>%
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
   paletteer::scale_fill_paletteer_d('NatParksPalettes::Olympic') +
   labs(y = str_wrap('Annual average generation exported (MWh/yr)', width = 12)) +
-  scale_y_continuous(limits = c(0.00, 3000000.0), labels = scales::comma) +
+  scale_y_continuous(limits = c(0.00, 2000000.0), labels = scales::comma) +
   theme_classic() +
   theme(legend.position = 'non',
         legend.title = element_blank(),
@@ -125,19 +125,48 @@ fig_1_6 <- no_ptc_scenarios %>%
 fig_1_7 <- ptc_scenarios %>%
   ggplot(aes(y = pct_batteryCap, x = scenario, group = scenario, fill = tx_availability)) + 
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
-  ylim(c(0.00, 100.0)) +
+  ylim(c(-15, 75)) +
   paletteer::scale_fill_paletteer_d('NatParksPalettes::Olympic') +
-  labs(y = str_wrap('Battery capacity as percent of solar capacity', width = 12), x = 'Scenario') +
+  labs(y = str_wrap('Battery capacity as percent of solar capacity', width = 12)) +
   theme_classic() +
   theme(legend.position = 'non',
-        legend.title = element_blank())
+        legend.title = element_blank(),
+        axis.title.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.x = element_blank())
 
 fig_1_8 <- no_ptc_scenarios %>%
   ggplot(aes(y = pct_batteryCap, x = scenario, group = scenario, fill = tx_availability)) + 
   geom_boxplot(alpha = 0.7, outlier.shape = NA) +
-  ylim(c(0.00, 100.0)) +
+  ylim(c(-15, 75)) +
   paletteer::scale_fill_paletteer_d('NatParksPalettes::Olympic') +
-  labs(y = str_wrap('Battery capacity as percent of solar capacity', width = 12), x = 'Scenario') +
+  labs(y = str_wrap('Battery capacity as percent of solar capacity', width = 12)) +
+  theme_classic() +
+  theme(legend.position = 'non',
+        legend.title = element_blank(),
+        axis.title.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        axis.text.x = element_blank(),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank())
+
+# Box and whisker plot for curtailment ----
+fig_1_9 <- ptc_scenarios %>%
+  ggplot(aes(y = curtailment_c, x = scenario, group = scenario, fill = tx_availability)) + 
+  geom_boxplot(alpha = 0.7, outlier.shape = NA) +
+  paletteer::scale_fill_paletteer_d('NatParksPalettes::Olympic') +
+  labs(y = str_wrap('Curtailment', width = 12), x = 'Scenario') +
+  scale_y_continuous(limits = c(0.00, 0.35), labels = scales::comma) +
+  theme_classic() +
+  theme(legend.position = 'non',
+        legend.title = element_blank())
+
+fig_1_10 <- no_ptc_scenarios %>%
+  ggplot(aes(y = curtailment_c, x = scenario, group = scenario, fill = tx_availability)) + 
+  geom_boxplot(alpha = 0.7, outlier.shape = NA) +
+  paletteer::scale_fill_paletteer_d('NatParksPalettes::Olympic') +
+  labs(y = str_wrap('Curtailment', width = 12), x = 'Scenario') +
+  scale_y_continuous(limits = c(0.00, 0.35), labels = scales::comma) +
   theme_classic() +
   theme(legend.position = 'bottom',
         legend.title = element_blank(),
@@ -149,5 +178,6 @@ row_1 <- fig_1_1 + fig_1_2
 row_2 <- fig_1_3 + fig_1_4
 row_3 <- fig_1_5 + fig_1_6
 row_4 <- fig_1_7 + fig_1_8
+row_5 <- fig_1_9 + fig_1_10
 
-row_1 / row_2 / row_3 / row_4
+row_1 / row_2 / row_3 / row_4 / row_5
